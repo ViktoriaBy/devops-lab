3ddc2136192b7702889bc2812d815417fb27a6e4
ViktoriaB <v.chamborant@hotmail.com>
2022-02-10 14:05:54 -0600

    added heroku stuff

--- server/server.js
+++ server/server.js
@@ -4,6 +4,20 @@ const app = express()
 
 const path = require('path');
 
+
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html')) //path.join joins heroku path(__dirname) with ours
+})
+
+
+
+
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR_TOKEN,
