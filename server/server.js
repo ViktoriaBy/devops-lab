d76d3ae53a25048beeeac069dcd6ea984a07339a
ViktoriaB <v.chamborant@hotmail.com>
2022-02-10 15:13:00 -0600

    changed to to do list

--- server/server.js
+++ server/server.js
@@ -12,10 +12,36 @@ app.use(cors())
 app.use(express.static('public'))
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html')) //path.join joins heroku path(__dirname) with ours
+
+const todos = ['Clean the house', 'Walk the dog', 'Eat some candy', 'Water the plants']
+
+//ENDPOINT
+app.get('/api/todos', (req, res) => {
+    rollbar.info('Someone got all the students')
+    res.status(200).send(todos)
 })
 
+app.post('/api/todos', (req, res) => {
+    const { name } = req.body
+    todos.unshift(name) //put new name at beginning at array
+
+    res.status(200).send(todos)
+})
+
+app.delete('/api/todos/:id', (req, res) => {
+    if (req.params.id === '0'){
+        rollbar.error('Someone tried to delete the first student!')
+        return res.status(403).send(todos)
+    }
+
+    rollbar.info(`Someone deleted student ${todos[+req.params.id]}`)
+
+    todos.splice(+req.params.id, 1) //DELETES STUDENT ID FROM ARRAY
+
+    
+
+    res.status(200).send(todos)
+})
 
 
 
@@ -26,7 +52,7 @@ const rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 
-
+rollbar.log("Hello world!");
 
 app.use(express.json())
 
